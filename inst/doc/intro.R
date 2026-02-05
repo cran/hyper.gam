d51018777b30f01cd4ffdb74b30a8710c011a041
Tingting Zhan <tingtingzhan@gmail.com>
2026-02-05 23:30:02 +0000

    version 0.2.2

--- inst/doc/intro.R
+++ inst/doc/intro.R
@@ -1,13 +1,4 @@
 ## -----------------------------------------------------------------------------
-#| code-fold: true
-#| code-summary: "Environment on author's computer"
-#| label: author-env
-Sys.info()[c('sysname', 'release', 'machine')]
-R.version
-
-
-## -----------------------------------------------------------------------------
 #| eval: false
-# utils::install.packages('groupedHyperframe')
 # utils::install.packages('hyper.gam')
 
